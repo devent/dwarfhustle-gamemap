4cb0dd6fb3074f3e7410b396633149e6c63840cc
Erwin Müller <erwin@muellerpublic.de>
2023-03-28 21:17:01 +0200

    Add tiles textures

---
+++ dwarfhustle-gamemap-assets/src/main/resources/TexturesMap.groovy
@@ -0,0 +1,61 @@
+import org.eclipse.collections.impl.factory.primitive.LongObjectMaps
+
+materialsTextures = LongObjectMaps.mutable.empty()
+materialsTextures[816] = "Textures/tiles/sedimentary/siltstone/siltstone-01.png" // siltstone
+materialsTextures[815] = "Textures/tiles/sedimentary/shale/shale-1-1024.png" // shale
+materialsTextures[814] = "Textures/tiles/sedimentary/sandstone/sandstone-06.png" // sandstone
+materialsTextures[813] = "Textures/tiles/sedimentary/rock_salt/rock_salt-1-1024.png" // rock_salt
+materialsTextures[812] = "Textures/tiles/sedimentary/mudstone/placeholder-1-1024.png" // mudstone
+materialsTextures[811] = "Textures/tiles/sedimentary/limestone/placeholder-1-1024.png" // limestone
+materialsTextures[810] = "Textures/tiles/sedimentary/dolomite/placeholder-1-1024.png" // dolomite
+materialsTextures[809] = "Textures/tiles/sedimentary/conglomerate/conglomerate-1-1024.png" // conglomerate
+materialsTextures[808] = "Textures/tiles/sedimentary/claystone/claystone-1-1024.png" // claystone
+materialsTextures[807] = "Textures/tiles/sedimentary/chert/chert-1-1024.png" // chert
+materialsTextures[806] = "Textures/tiles/sedimentary/chalk/chalk-1-1024.png" // chalk
+// IgneousIntrusive
+materialsTextures[820] = "Textures/tiles/igneous_intrusive/granite/granite-2-1024.png" // granite
+materialsTextures[819] = "Textures/tiles/igneous_intrusive/gabbro/gabbro-1-1024.png" // gabbro
+materialsTextures[818] = "Textures/tiles/igneous_intrusive/diorite/diorite-1-1024.png" // diorite
+// IgneousExtrusive
+materialsTextures[826] = "Textures/tiles/igneous_extrusive/rhyolite/" // rhyolite
+materialsTextures[825] = "Textures/tiles/igneous_extrusive/obsidian/" // obsidian
+materialsTextures[824] = "Textures/tiles/igneous_extrusive/dacite/" // dacite
+materialsTextures[823] = "Textures/tiles/igneous_extrusive/basalt/" // basalt
+materialsTextures[822] = "Textures/tiles/igneous_extrusive/andesite/" // andesite
+// Metamorphic
+materialsTextures[833] = "Textures/tiles/metamorphic/slate/" // slate
+materialsTextures[832] = "Textures/tiles/metamorphic/schist/" // schist
+materialsTextures[831] = "Textures/tiles/metamorphic/quartzite/" // quartzite
+materialsTextures[830] = "Textures/tiles/metamorphic/phyllite/" // phyllite
+materialsTextures[829] = "Textures/tiles/metamorphic/marble/" // marble
+materialsTextures[828] = "Textures/tiles/metamorphic/gneiss/" // gneiss
+// special
+materialsTextures[804] = "Textures/tiles/special_stone_layer/magma/magma-01.png" // magma
+// soil
+materialsTextures[883] = "Textures/tiles/soil/" // silt_loam
+materialsTextures[882] = "Textures/tiles/soil/" // silty_clay_loam
+materialsTextures[881] = "Textures/tiles/soil/" // silt
+materialsTextures[880] = "Textures/tiles/soil/" // sandy_loam
+materialsTextures[879] = "Textures/tiles/soil/" // sandy_clay_loam
+materialsTextures[878] = "Textures/tiles/soil/" // peat
+materialsTextures[877] = "Textures/tiles/soil/" // loamy_sand
+materialsTextures[876] = "Textures/tiles/soil/" // loam
+materialsTextures[874] = "Textures/tiles/soil/" // calcareous_ooze
+materialsTextures[873] = "Textures/tiles/soil/" // siliceous_ooze
+materialsTextures[872] = "Textures/tiles/soil/" // pelagic_clay
+materialsTextures[870] = "Textures/tiles/soil/" // yellow_sand
+materialsTextures[869] = "Textures/tiles/soil/" // white_sand
+materialsTextures[868] = "Textures/tiles/soil/" // red_sand
+materialsTextures[867] = "Textures/tiles/soil/" // black_sand
+materialsTextures[778] = "Textures/tiles/soil/" // sand
+materialsTextures[864] = "Textures/tiles/soil/" // fire_clay
+materialsTextures[862] = "Textures/tiles/soil/" // silty_clay
+materialsTextures[860] = "Textures/tiles/soil/" // sandy_clay
+materialsTextures[858] = "Textures/tiles/soil/" // clay_loam
+materialsTextures[779] = "Textures/tiles/soil/" // clay
+// gas
+materialsTextures[891] = "Textures/tiles/gas/sulfur_dioxide/sulfur_dioxide-1-1024.png" // sulfur_dioxide
+materialsTextures[890] = "Textures/tiles/gas/carbon_dioxide/carbon_dioxide-1-1024.png" // carbon_dioxide
+materialsTextures[888] = "Textures/tiles/gas/polluted_oxygen/polluted_oxygen-1-1024.png" // polluted_oxygen
+materialsTextures[886] = "Textures/tiles/gas/oxygen/oxygen-01.png" // oxygen
+materialsTextures[885] = "Textures/tiles/gas/vacuum/vacuum-01.png" // vacuum
