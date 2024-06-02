f7749f51f1b18b3b2a759253d78e414e4a9c718e
Erwin Müller <erwin@muellerpublic.de>
2024-06-02 10:51:35 +0200

    Update license

--- dwarfhustle-gamemap-jme/src/main/java/com/anrisoftware/dwarfhustle/gamemap/jme/app/AbstractAssetsCacheActor.java
+++ dwarfhustle-gamemap-jme/src/main/java/com/anrisoftware/dwarfhustle/gamemap/jme/app/AbstractAssetsCacheActor.java
@@ -1,5 +1,5 @@
 /*
- * dwarfhustle-model-db - Manages the compile dependencies for the model.
+ * dwarfhustle-gamemap-jme - Game map.
  * Copyright © 2023 Erwin Müller (erwin.mueller@anrisoftware.com)
  *
  * This program is free software: you can redistribute it and/or modify
